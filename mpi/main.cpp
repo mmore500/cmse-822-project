24054eb7745f6e9f7cb6c8b72c51cc5ee1f4eeea
Matthew Andres Moreno <matthew.andres.moreno@gmail.com>
2018-11-20 11:23:40 -0500

    Add MPI template

---
+++ mpi/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <mpi.h>
+#include <vector>
+
+int main(int argc, char *argv[])
+{
+  std::cout << "hello world" << std::endl;
+  return 0;
+}
