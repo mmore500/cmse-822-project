2506412b9e80e8d9b1edef91f89e259b02d5ee9b
Matthew Andres Moreno <matthew.andres.moreno@gmail.com>
2018-11-28 16:24:14 -0500

    Symlink useful headers from charm

---
+++ mpi/Config.h
@@ -0,0 +1,1 @@
+../charm/Config.h
--- mpi/main.cpp
+++ mpi/main.cpp
@@ -4,6 +4,9 @@
 #include <mpi.h>
 #include <vector>
 
+#include "cardinal.h"
+#include "distance.h"
+
 int main(int argc, char *argv[])
 {
   std::cout << "hello world" << std::endl;
